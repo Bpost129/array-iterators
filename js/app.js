c36ce3754f568d0b17a1f5421765c020908841fb
Bpost129 <Bpost129@yahoo.com>
2025-02-04 22:45:33 -0500

    complete exercises 10 & 11

--- js/app.js
+++ js/app.js
@@ -142,13 +142,17 @@ const comments = [
 // Array.prototype.find()
 // 10. Find the comment with the id of 823423.
 
-
+const findId = comments.find(com => {
+  return com.id === 823423
+})
 
 console.log(findId)
 
 // Array.prototype.findIndex()
 // 11. Find the index of the comment with an id of 123523.
 
-
+const findCommentIdx = comments.findIndex(com => {
+  return com.id === 123523
+})
 
 console.log(findCommentIdx)
